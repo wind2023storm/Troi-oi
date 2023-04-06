9270dea18880880beac3e43a8ed6720bdb8ab5f2
Salman Ahmed <shahsalmanahmed009@gmail.com>
2023-04-06 11:12:39 +0600

    Removed social links

--- components/Sidebar/SidebarBottom.jsx
+++ components/Sidebar/SidebarBottom.jsx
@@ -1,35 +1,3 @@
-import Image from "next/image";
-import Link from "next/link";
-import facebook from "../../assets/images/facebook.svg";
-import instagram from "../../assets/images/instagram.svg";
-import linkedin from "../../assets/images/linkedin.svg";
-import twitter from "../../assets/images/twitter.svg";
-
 export default function SidebarBottom() {
-  return (
-    <div className="flex">
-      <ul className="flex gap-2 justify-center items-center w-full py-6 text-zinc-400">
-        <li className="dark:bg-slate-800 bg-slate-600 p-2.5 rounded-full">
-          <Link href="https://www.instagram.com/troioi.agency/" target="_blank">
-            <Image src={instagram} alt="instagram" className="w-5" />
-          </Link>
-        </li>
-        <li className="dark:bg-slate-800 bg-slate-600 p-2.5 rounded-full">
-          <Link href="https://twitter.com/TroiOi_Agency" target="_blank">
-            <Image src={twitter} alt="twitter" className="w-5" />
-          </Link>
-        </li>
-        <li className="dark:bg-slate-800 bg-slate-600 p-2.5 rounded-full">
-          <Link href="https://www.facebook.com/troioi.io" target="_blank">
-            <Image src={facebook} alt="facebook" className="w-5" />
-          </Link>
-        </li>
-        <li className="dark:bg-slate-800 bg-slate-600 p-2.5 rounded-full">
-          <Link href="https://www.linkedin.com/company/troi-oi" target="_blank">
-            <Image src={linkedin} alt="linkedin" className="w-5" />
-          </Link>
-        </li>
-      </ul>
-    </div>
-  );
+  return <div className="flex"></div>;
 }
